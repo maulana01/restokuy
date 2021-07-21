2af385be6a63887f74673758a5f40bb37163b243
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-07-21 23:11:11 +0700

    fix merge

--- index.php
+++ index.php
@@ -36,15 +36,9 @@
             </div>
         </div>
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <!-- <script src=" ./js/bootstrap.bundle.min.js">
-    </script> -->
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <script src="./js/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-
+    <script src=" ./js/bootstrap.bundle.min.js"></script>
 
 </body>
 
