3104ea9e29526194a2fb03855342912e1c455e10
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-07-21 23:01:04 +0700

    membuat login-page

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/theme.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Restokuy</title>
+</head>
+
+<body class="login">
+    <div class="login-page">
+        <div class="container">
+            <div class="card position-absolute top-50 start-50 translate-middle">
+                <div class="card-body p-4">
+                    <h1 class="card-title text-center mb-4">LOGIN</h1>
+                    <form action="" method="post">
+                        <div class="mb-3">
+                            <label for="exampleInputUsername1" class="form-label">Username</label>
+                            <input type="username" class="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp">
+                        </div>
+                        <div class="mb-4">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="d-grid justify-content-center">
+                            <button type="submit" class="btn btn-large bg--primary"><span class="font-btn font-white">Login</span></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- <script src=" ./js/bootstrap.bundle.min.js">
+    </script> -->
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
