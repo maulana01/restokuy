bd5dedbafdaac1079ba47c4106f60026385f7a32
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-07-23 18:13:22 +0700

    merubah link css

--- index.php
+++ index.php
@@ -9,24 +9,24 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/theme.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Restokuy</title>
 </head>
 
 <body class="login">
     <div class="login-page">
-        <div class="container">
+        <!-- <div class="container-fluid"> -->
             <div class="card position-absolute top-50 start-50 translate-middle">
                 <div class="card-body p-4">
                     <h1 class="card-title text-center mb-4">LOGIN</h1>
                     <form action="" method="post">
                         <div class="mb-3">
                             <label for="exampleInputUsername1" class="form-label">Username</label>
-                            <input type="username" class="form-control" id="exampleInputUsername1" aria-describedby="usernameHelp">
+                            <input type="username" class="form-control" id="exampleInputUsername1" name="username" required>
                         </div>
                         <div class="mb-4">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
                         </div>
                         <div class="d-grid justify-content-center">
                             <button type="submit" class="btn btn-large bg--primary"><span class="font-btn font-white">Login</span></button>
@@ -34,7 +34,7 @@
                     </form>
                 </div>
             </div>
-        </div>
+        <!-- </div> -->
     </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
