660eeb626cb011894b008f19eab5b9737d49a9fd
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2021-07-26 11:43:36 +0700

    membuat tampilan pegawai

---
+++ public/__tampilan/view/admin/pegawai/pegawai.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="./../../../css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="./../../../css/theme.css" type="text/css">
+  <link rel="stylesheet" href="./../../../css/main.css" type="text/css">
+  <title>Restokuy</title>
+</head>
+
+<body>
+  <header class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg--four">
+      <div class="container-fluid">
+        <button class="btn btn-sm" id="btn-sidebar">
+          <img src="./../../../img/hamburger-menu.svg" alt="Menu" class="font-primary">
+        </button>
+        <div class="d-flex">
+          <a href="dashboard.php" class="navbar-brand font-primary navbar-title">RestoKuy</a>
+        </div>
+        <form method="post">
+          <div class="dropdown">
+            <a role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="./../../../img/icon-profile.svg" alt="profile">
+            </a>
+            <ul class="dropdown-menu dropdown-menu-lg-end dropdown-color" aria-labelledby="dropdownMenuLink">
+              <li><span class="dropdown-item-text font-primary fw-normal">Dandi</span></li>
+              <li><button class="dropdown-item font-primary fw-normal" name="btn-keluar">Keluar</button></li>
+            </ul>
+          </div>
+        </form>
+      </div>
+    </nav>
+  </header>
+
+  <main class="container-fluid">
+    <div class="row">
+      <div class="col-2 bg--third side" id="sidebars">
+        <ul class="nav flex-column my-3 nav-sidebar">
+          <li>
+            <a href="./../dashboard.php" class="nav-link font-primary">
+              <img src="./../../../img/icon-dashboard.svg" alt="Dashboard" class="pb-2">
+              <span class="mx-2 fw-bold">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="" class="nav-link disabled"><span class="font-four fw-bold">Master File</span></a>
+          </li>
+          <li>
+            <a href="./pegawai.php" class="nav-link font-primary">
+              <img src="./../../../img/icon-pegawai-dashboard.svg" alt="Dashboard" class="pb-2">
+              <span class="mx-2 fw-bold">Pegawai</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link font-primary">
+              <img src="./../../../img/icon-menu-sidebar.svg" alt="Dashboard" class="pb-2">
+              <span class="mx-2 fw-bold">Menu</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col m-5">
+
+        <h2 class="font-primary">Informasi Pegawai</h2>
+        <a href="./pegawai-tambah.php" class="btn font-btn bg--third font-white my-4">Tambah</a>
+        <table class="table table-bordered table-hover">
+          <thead class="table-light">
+            <tr>
+              <th>ID Pegawai</th>
+              <th>Nama</th>
+              <th>Tempat, Tanggal Lahir</th>
+              <th>Nomor Telepon</th>
+              <th>Alamat</th>
+              <th>Jabatan</th>
+              <th>Username</th>
+              <th colspan="2" class="text-center">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Foreach -->
+            <tr>
+              <td>P0001</td>
+              <td>Dandi Ahmadin</td>
+              <td>Amerika, 10-10-2020</td>
+              <td>089123321123</td>
+              <td>Jl. Sayang Kamu</td>
+              <td>Kasir</td>
+              <td>Kdandi</td>
+              <td class="text-center">
+                <a href="" class="btn btn-sm bg--four font-btn font-white">Edit</a>
+                <button class="btn btn-sm bg--primary font-btn font-white" data-bs-toggle="modal" data-bs-target="#hapusModal">Hapus</button>
+              </td>
+            </tr>
+            <!-- foreach -->
+            <tr>
+              <td>P0001</td>
+              <td>Dandi Ahmadin</td>
+              <td>Amerika, 10-10-2020</td>
+              <td>089123321123</td>
+              <td>Jl. Sayang Kamu</td>
+              <td>Kasir</td>
+              <td>Kdandi</td>
+              <td class="text-center">
+                <a href="" class="btn btn-sm bg--four font-btn font-white">Edit</a>
+                <button class="btn btn-sm bg--primary font-btn font-white" data-bs-toggle="modal" data-bs-target="#hapusModal">Hapus</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- Modal -->
+        <div class="modal fade" id="hapusModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <img src="../../../img/question-circle-fill.svg" alt="question">
+                <h5 class="modal-title ms-2" id="exampleModalLabel">Konfirmasi Hapus Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <p>Apakah Anda Yakin Ingin Menghapus ?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-font bg--primary font-white" data-bs-dismiss="modal">Ya</button>
+                <button type="button" class="btn btn-font bg--four font-white" data-bs-dismiss="modal">Tidak</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row my-5">
+        <div class="col">
+
+        </div>
+      </div>
+    </div>
+    </div>
+  </main>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <!-- <script src="./../../js/bootstrap.bundle.min.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script>
+    // $('#btn-sidebar').click(function () {
+    //   $('#sidebar').hide();
+    // })
+    document.getElementById('btn-sidebar').addEventListener('click', function() {
+      document.getElementById('sidebars').classList.toggle('side');
+    })
+  </script>
+</body>
+
+</html>
